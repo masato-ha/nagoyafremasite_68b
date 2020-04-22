c8c12a1cb5516b1dc5480fb7bd8440171ce2b1e6
Ryo-cool <coolboxing576@gmail.com>
2020-04-22 17:55:20 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_041655) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "introduction", null: false
-    t.integer "price", null: false
-    t.string "item_condition", null: false
-    t.string "shipping_area", null: false
-    t.string "size", null: false
-    t.datetime "preparation_day", null: false
-    t.string "trading_status", null: false
-    t.string "postage_type", null: false
-    t.datetime "deal_closed_date"
-    t.integer "brand_id"
-    t.integer "buyer_id", null: false
-    t.integer "seller_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
