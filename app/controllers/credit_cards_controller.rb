280c39ae35cf6043a4a947fbf95c889827c6e589
kdog0625 <kdoooog34@gmail.com>
2020-04-26 10:37:37 +0900

    マイページ実装完了

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,9 @@
 class CreditCardsController < ApplicationController
   def index
+    @CreditCards=CreditCard.all
+    @CreditCard=CreditCard.new
+  end
+  def new
+    @CreditCard=CreditCard.new
   end
 end
