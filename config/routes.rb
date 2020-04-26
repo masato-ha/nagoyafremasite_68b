c949fc87c927889100318c255f6759bd1704475b
kdog0625 <kdoooog34@gmail.com>
2020-04-26 14:26:00 +0900

    route修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'credit_cards/index'
+  # get 'credit_cards/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # devise_for :users
   # get 'users/index'
