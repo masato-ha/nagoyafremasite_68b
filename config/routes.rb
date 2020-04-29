3215cf96f9623330eecc14efd9d9d70a1b485d73
masanari-nakamichi <shirohana2627@gmail.com>
2020-04-29 19:17:33 +0900

    make-new-and-login-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # get 'credit_cards/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # devise_for :users
-  # get 'users/index'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  
+  # devise_scope :user do
+  #   get "user/:id", :to => "users/registrations#detail"
+  #   get "signup", :to => "users/registrations#new"
+  #   get "login", :to => "users/sessions#new"
+  #   get "logout", :to => "users/sessions#destroy"
+  # end
   root 'items#index'
   resources :users
   resources :credit_cards
