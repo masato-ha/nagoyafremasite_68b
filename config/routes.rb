54cdbc6f0c59b64d7604ce37475a144674027c3f
Ryo-cool <coolboxing576@gmail.com>
2020-05-01 13:39:58 +0900

    conflict-fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,25 @@ Rails.application.routes.draw do
   # devise_for :users
   # get 'users/index'
   root 'items#index'
+  <<<<<<< create-exhibition-views
   resources :users
   resources :credit_cards
   resources :items
+=======
+  resources :users do
+   resource :logout ,only: [:show]
+  end
+  resources :credit_cards do
+    collection do
+      get 'about'
+    end
+  end
+
+
+  resource :mypages do
+    
+  end
+  get 'logouts/show'
+  # get 'mypages/show'
+>>>>>>> master
 end
