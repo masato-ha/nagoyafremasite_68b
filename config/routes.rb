13c3124d8b837272da51b5cddc169fd3cca8de49
Ryo-cool <coolboxing576@gmail.com>
2020-05-01 13:43:53 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  
+  
   # get 'credit_cards/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # devise_for :users
-  # get 'users/index'
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  
   root 'items#index'
-  resources :users
-  resources :credit_cards
-  resources :items
   resources :users do
    resource :logout ,only: [:show]
   end
