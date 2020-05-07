8d9d59391ebfb42793f6fa76ea49c46ffb4cc31f
masanari-nakamichi <shirohana2627@gmail.com>
2020-05-07 17:58:22 +0900

    fix-function

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Fremasite
     config.load_defaults 5.2
     config.logger = Logger.new(STDOUT)
 
+    config.i18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
