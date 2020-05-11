aebec6e51ca25aad2b1e0d42ab11639f3376665f
Ryo-cool <coolboxing576@gmail.com>
2020-05-11 19:37:03 +0900

    複数画像投稿

--- app/assets/javascripts/items_new.js
+++ app/assets/javascripts/items_new.js
@@ -1,24 +1,24 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="items[images_attributes][${index}][url]"
-                    id="images_attributes_${index}_url"><br>
-                    <div class="js-remove">削除</div>
+    const html = `<div data-index="${index}" class="js-file_group"> 
+                    <input class="js-file" type="file"style="display: none;"
+                    name="item[images_attributes][${index}][url]"
+                    id="item_images_attributes_${index}_url"><br>
                   </div>`;
     return html;
   }
-  
+
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
+  // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
 
   $('.hidden-destroy').hide();
 
-  $('#image-box').on('change', '.js-file', function(e) {
+  $('#image-input').on('change', '.js-file', function(e) {
+    $('.sell-container__content__upload__items__box__label').prop("for", `item_images_attributes_${fileIndex[0]}_url`)
     const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
@@ -27,59 +27,16 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
-    } else {  // 新規画像追加の処理
-      $('#previews').append(buildImg(targetIndex, blobUrl));
+    } else {  
       // fileIndexの先頭の数字を使ってinputを作る
-      $('#image-box').append(buildFileField(fileIndex[0]));
+      $('#image-input').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
+      console.log
       // 末尾の数に1足した数を追加する
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
     }
   });
 
-
-  $('#image-box').on('click', '.js-remove', function() {
-    $(this).parent().remove();
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  });
-    // 画像が選択された時プレビュー表示、inputの親要素のdivをイベント元に指定
-    $('#image-input').on('change', function(e){
-
-      //ファイルオブジェクトを取得する
-      let files = e.target.files;
-      $.each(files, function(index, file) {
-        let reader = new FileReader();
-  
-        //画像でない場合は処理終了
-        if(file.type.indexOf("image") < 0){
-          alert("画像ファイルを指定してください。");
-          return false;
-        }
-        //アップロードした画像を設定する
-        reader.onload = (function(file){
-          return function(e){
-            let imageLength = $('#output-box').children('li').length;
-            // 表示されているプレビューの数を数える
-  
-            let labelLength = $("#image-input>label").eq(-1).data('label-id');
-            // #image-inputの子要素labelの中から最後の要素のカスタムデータidを取得
-  
-            // プレビュー表示
-            $('#image-input').before(`<li class="preview-image" id="upload-image${labelLength}" data-image-id="${labelLength}">
-                                        <figure class="preview-image__figure">
-                                          <img src='${e.target.result}' title='${file.name}' >
-                                        </figure>
-                                        <div class="preview-image__button">
-                                          <a class="preview-image__button__edit">編集</a>
-                                          <a class="preview-image__button__delete" data-image-id="${labelLength}">削除</a>
-                                        </div>
-                                      </li>`);
-          };
-        })(file);
-        reader.readAsDataURL(file);
-      });
-    });
 
   //削除ボタンが押された時
   $(document).on('click', '.preview-image__button__delete', function(){
@@ -132,18 +89,18 @@ $(document).on('turbolinks:load', ()=> {
     });
 
     //送信しようとした時
-    $('form').on('submit',function(){
-      let imageLength = $('#output-box').children('li').length;
-      if(imageLength ==''){
-        $('body, html').animate({ scrollTop: 0 }, 500);
-        $('#error-image').text('画像がありません');
-      }else if(imageLength >10){
-        $('body, html').animate({ scrollTop: 0 }, 500);
-        $('#error-image').text('画像を10枚以下にして下さい');
-      }else{
-        return true;
-      }
-    });
+    // $('form').on('submit',function(){
+    //   let imageLength = $('#output-box').children('li').length;
+    //   if(imageLength ==''){
+    //     $('body, html').animate({ scrollTop: 0 }, 500);
+    //     $('#error-image').text('画像がありません');
+    //   }else if(imageLength >10){
+    //     $('body, html').animate({ scrollTop: 0 }, 500);
+    //     $('#error-image').text('画像を10枚以下にして下さい');
+    //   }else{
+    //     return true;
+    //   }
+    // });
 
      //画像を削除した時
     $(document).on('click','.preview-image__button__delete',function(){
@@ -241,5 +198,3 @@ $(document).on('turbolinks:load', ()=> {
     });
   });  
 });
-
-
