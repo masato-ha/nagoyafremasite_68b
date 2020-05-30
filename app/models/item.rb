a745948536525d9b0bade3c417b03140b2b31507
kdog0625 <kdoooog34@gmail.com>
2020-05-30 09:35:17 +0900

    soldビュー

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   # belongs_to :brand #optional: true
   belongs_to :user
   has_many :images
+  
   # belongs_to :brand
   # belongs_to :user #optional: true#through:comments
   #画像複数投稿
