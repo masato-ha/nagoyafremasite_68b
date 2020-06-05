878e543b55e235a8c0580c62b4bb3a0abed90498
”Hadama＂ <email@example.com>
2020-06-05 20:41:50 +0900

    商品編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,13 +19,26 @@ class ItemsController < ApplicationController
   end
 
   def show
-   
+
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_update_params)
+    if @item
+      redirect_to root_path
+    else 
+      render edit
+    end
+      # if  @item.update_attributes(item_update_params)
+        #  redirect_to "/"
+      # else
+        # render action: :edit
+      # end
   end
 
   def destroy
@@ -44,7 +57,11 @@ class ItemsController < ApplicationController
   def item_params
      params.require(:item).permit(:name, :price, :introduction, :category_id, :item_condition, :brand_id, :shipping_area, :preparation_day, :trading_status, :postage_type, images_attributes: [:url]).merge(user_id: current_user.id)
   end
+  
+  def item_update_params
+    params.require(:item).permit(:name, :price, :introduction, :category_id, :item_condition, :brand_id, :shipping_area, :preparation_day, :trading_status, :postage_type, images_attributes: [:url, :id, :_destroy]).merge(user_id: current_user.id)
+      
+  end
 
   
-
 end
